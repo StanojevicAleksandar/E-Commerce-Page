8c7060c2eb6d80f0ae6f2eca356a156ce0d30960
StanojevicAleksandar <109697885+StanojevicAleksandar@users.noreply.github.com>
2022-08-13 02:04:40 +0200

    Fixed the Cart issue

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import EmptyCart from "../assets/empty_cart.svg"
-
-
+import EmptyCart from "../assets/empty_cart.svg";
 
 const Cart = ({ cart, changeQuantity, removeItem }) => {
   const total = () => {
     let price = 0;
     cart.forEach((item) => {
-      price += +(item.salePrice || item.originalPrice * item.quantity
-        ).toFixed(2);
+      price += +(item.salePrice || item.originalPrice * item.quantity).toFixed(
+        2
+      );
     });
     return price;
   };
+
   return (
     <div id="books__body">
       <div id="books__main">
@@ -45,7 +45,12 @@ const Cart = ({ cart, changeQuantity, removeItem }) => {
                             CHF
                             {(book.salePrice || book.originalPrice).toFixed(2)}
                           </span>
-                          <button className="cart__book--remove" onClick={() =>removeItem(book)}>Remove</button>
+                          <button
+                            className="cart__book--remove"
+                            onClick={() => removeItem(book)}
+                          >
+                            Remove
+                          </button>
                         </div>
                       </div>
                       <div className="cart__quantity">
@@ -70,37 +75,38 @@ const Cart = ({ cart, changeQuantity, removeItem }) => {
                   );
                 })}
               </div>
-              {
-                cart.length === 0 &&
-              <div className="cart__empty">
-                <img src={EmptyCart} alt="" className="cart__empty--img" />
-                <h2>You don't have any books in your cart!</h2>
-                <Link to="/books">
-                <button className="btn">Browse books</button>
-                </Link>
-              </div>
-              }
+              {cart.length === 0 && (
+                <div className="cart__empty">
+                  <img src={EmptyCart} alt="" className="cart__empty--img" />
+                  <h2>You don't have any books in your cart!</h2>
+                  <Link to="/books">
+                    <button className="btn">Browse books</button>
+                  </Link>
+                </div>
+              )}
             </div>
-           { cart.length && <div className="total">
-              <div className="total__item total__sub-total">
-                <span>Subtotal</span>
-                <span>CHF{(total() * 0.923).toFixed(2)}</span>
+            {cart.length && (
+              <div className="total">
+                <div className="total__item total__sub-total">
+                  <span>Subtotal</span>
+                  <span>CHF{(total() * 0.923).toFixed(2)}</span>
+                </div>
+                <div className="total__item total__tax">
+                  <span>Tax</span>
+                  <span>CHF{(total() * 0.077).toFixed(2)}</span>
+                </div>
+                <div className="total__item total__price">
+                  <span>Total</span>
+                  <span>CHF{total().toFixed(2)}</span>
+                </div>
+                <button
+                  className="btn btn__checkout no-cursor"
+                  onClick={() => alert(`Haven't got around to doing this :(`)}
+                >
+                  Proceed to checkout
+                </button>
               </div>
-              <div className="total__item total__tax">
-                <span>Tax</span>
-                <span>CHF{(total() * 0.077).toFixed(2)}</span>
-              </div>
-              <div className="total__item total__price">
-                <span>Total</span>
-                <span>CHF{total().toFixed(2)}</span>
-              </div>
-              <button
-                className="btn btn__checkout no-cursor"
-                onClick={() => alert(`Haven't got around to doing this :(`)}
-              >
-                Proceed to checkout
-              </button>
-            </div>} 
+            )}
           </div>
         </div>
       </div>
